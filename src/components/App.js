07f6b76a324115a67325dd6fee5bc84237383e80
Yaroslav Liakh <liakhyaroslav@gmail.com>
2018-03-29 21:51:31 +0300

    Some movements
    Create folders for stylesheet's and images
    
    Move `App` to `components`

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import HeaderComponent from "./HeaderComponent";
+
+import "../styles/App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <HeaderComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
