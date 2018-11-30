02f397e4a4963eeb8ef3283851d2e09a8042080b
Yaroslav Liakh <liakhyaroslav@gmail.com>
2018-11-30 13:29:16 +0200

    Refactor reducers & actions
    Added `updateProfile` reducer & action for handling user profile updating.
    
    Fixed issue, when credentials are stayed in `redux` after singin in.
    
    Changed `current_user`.
    
    Added `errorMessages` in `notice`.

---
+++ src/actions/signForm/clearSignFields.js
@@ -0,0 +1,5 @@
+export default function clearSignFields(inputValue) {
+  return {
+    type: "CLEAR_SIGN_FIELDS"
+  };
+}
--- src/reducers/notice.js
+++ src/reducers/notice.js
@@ -1,15 +1,19 @@
-import { logged } from "../utils/session/checkUser";
+import Token from "../services/session/localStorageProvider";
+
+let logged = Token.getToken();
 
 const initialState = {
   showSignInAlert: false,
-  showSuccessAlert: !logged
+  showSuccessAlert: !logged,
+  errorMessages: ""
 };
 
 export default function notice(state = initialState, action) {
   if (action.type === "SHOW_ERROR_ALERT") {
     return {
       ...state,
-      showSignInAlert: action.payload
+      showSignInAlert: action.payload,
+      errorMessages: action.payload.body
     };
   } else if (action.type === "SHOW_SUCCESS_ALERT") {
     return {
